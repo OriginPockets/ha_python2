d88536639b715a3272d1095eee98b57ab28d6c65
OriginPockets <104317559+OriginPockets@users.noreply.github.com>
2024-06-28 09:04:04 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,27 @@
+class Oyaneko:
+    def __init__(self, breed, color, tail, neko_skill):
+        self.breed = breed
+        self.color = color
+        self.tail = tail
+        self.neko_skill = neko_skill
+
+    def hissatu(self):
+        print(f'{self.breed}の必殺技{self.neko_skill}!')
+
+
+class Koneko(Oyaneko):
+    def __init__(self, color, tail, name, neko_skill):
+        super().__init__("マンチカン", color, tail, neko_skill)
+        self.name = name
+
+    # hissatsuメソッドをオーバーライド
+    def hissatsu(self):
+        print(f'{self.color}色の赤ちゃんマンチカンの必殺技{self.neko_skill}!')
+
+    # 新しいメソッド（飼い主にごあいさつ）の追加
+    def say_hello(self):
+        print(f'はじめまして。ワタシの名前は{self.name}だにゃ。')
+
+koneko1 = Koneko('オフホワイト', '短い', 'しろねこ', 'ねこじゃらし攻撃')
+koneko1.hissatsu()
+koneko1.say_hello()
